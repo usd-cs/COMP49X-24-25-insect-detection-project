a00d3756d9ce9c284e5faeb8b60af12fd470071d
Joe Cox <joecox@sandiego.edu>
2025-02-19 16:19:36 -0800

    Removed height instances as that will be handled in another class

--- src/model_loader.py
+++ src/model_loader.py
@@ -4,30 +4,22 @@ from torchvision import models
 import torch
 
 class TrainedModels:
-    def __init__(self, weights_file_paths = None, height_file_paths = None, test = False):
+    def __init__(self, weights_file_paths = None, test = False):
         """Initialize class variables. User can specify height parameter for model."""
+        self.weights_file_paths = weights_file_paths or {}
+
+        self.models = {
+            "caud" : None, 
+            "dors" : None,
+            "fron" : None,
+            "late" : None
+        }
+
+        # use CPU to allow general usability and Metal Performance Shader if user has Apple Silicon
+        self.device = torch.device("mps" if torch.backends.mps.is_built() else "cpu")
+        
         if not test:
-            self.weights_file_paths = weights_file_paths or {}
-            self.height_file_paths = height_file_paths or {}
-            self.heights = {} # Dictionary to store heights per model
-
-            self.models = {
-                "caud" : None, 
-                "dors" : None,
-                "fron" : None,
-                "late" : None
-            }
-
-            # use CPU to allow general usability and Metal Performance Shader if user has Apple Silicon
-            self.device = torch.device("mps" if torch.backends.mps.is_built() else "cpu")
-            
             self.model_initializer()
-        else:
-            self.weights_file_paths = weights_file_paths
-            self.height_file_paths = height_file_paths
-            self.models = {}
-            self.heights = {} 
-            self.device = torch.device("cpu")
     
     def model_initializer(self):
         for key in self.models:
@@ -52,23 +44,10 @@ class TrainedModels:
             weights_file_path = input(f"Please input the file path of the saved weights for the {key} trained model: ")
         else:
             weights_file_path = self.weights_file_paths[key]
-        if not self.height_file_paths:
-            height_file_path = input(f"Please input the file path of the saved height for the {key} image input: ")
-        else:
-            height_file_path = self.height_file_paths[key]
-        
-        # Load height from file path
-        try:
-            height_file = open(height_file_path, 'r')
-            self.heights[key] = int(height_file.readline().strip())
-        except (FileNotFoundError, ValueError):
-            print(f"Height File for {key} Model Does Not Exist.")
-            return
 
         # Load weights from file path into the model
         try:
-            self.models[key].load_state_dict(torch.load(weights_file_path, map_location=self.device))
-            # self.models[key].load_state_dict(torch.load(weights_file_path, map_location=self.device))
+            self.models[key].load_state_dict(torch.load(weights_file_path, map_location=self.device, weights_only=True))
         except FileNotFoundError:
             print(f"Weights File for {key} Model Does Not Exist.")
             return
