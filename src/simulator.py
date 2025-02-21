a8d7709c2fe3dff8d81e03f77a8c69d42cb0a473
danieldaugbjergusd <ddaugbjerg@sandiego.edu>
2025-02-20 16:55:58 -0800

    debugged training programs and implemented almost complete end to end test in sim

--- src/simulator.py
+++ src/simulator.py
@@ -1,12 +1,16 @@
+import sys
+import os
 from training_data_converter import TrainingDataConverter
 from training_database_reader import DatabaseReader
 from training_program import TrainingProgram
+from model_loader import ModelLoader
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
 # simple simulation of end-to-end functionality of files
 
 if __name__ == '__main__':
     # Set up data converter
-    tdc = TrainingDataConverter("../dataset/")
+    tdc = TrainingDataConverter("dataset")
     tdc.conversion("training.db")
     # Read converted data
     dbr = DatabaseReader("training.db")
@@ -23,4 +27,13 @@ if __name__ == '__main__':
     # Save models
     tp.save_models("caud.pth", "dors.pth", "fron.pth", "late.pth", "height.txt")
 
-    
+    # Load models
+    model_paths = {
+            "caud" : "src/models/caud.pth", 
+            "dors" : "src/models/dors.pth",
+            "fron" : "src/models/fron.pth",
+            "late" : "src/models/late.pth"
+        }
+    ml = ModelLoader(model_paths)
+    print(ml.get_models().keys)
+    print(ml.get_model("caud").named_parameters())
