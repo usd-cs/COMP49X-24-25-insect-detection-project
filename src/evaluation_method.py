e6e52bf898c05fa661cec6983b58e12849fe2072
Thomas McKeown <tmckeown@sandiego.edu>
2025-02-20 17:42:44 -0800

    Some tests and most of integration code in place. Two more test cases still in the works

--- src/evaluation_method.py
+++ src/evaluation_method.py
@@ -2,7 +2,8 @@
 Method that takes in a user input image or set of user images and runs them through 
 the loaded trained models and creates a combined classification output
 """
-from torchvision import transforms
+from torchvision import transforms, models
+import torch
 
 class EvaluationMethod:
     """
@@ -10,7 +11,7 @@ class EvaluationMethod:
     loaded CNN models
     """
 
-    def __init__(self, height_filename):
+    def __init__(self, height_filename, models_dict):
         """
         Load the trained models for usage and have the class prepared for user input.
         During testing phases, determining which evaluation method defined below will 
@@ -20,12 +21,11 @@ class EvaluationMethod:
 
         #weights for use in weighted eval. Can be tweaked later to optimize evaluation accuracy
         self.weights = [0.25, 0.25, 0.25, 0.25]
+        self.trained_models = models_dict
 
         self.height = None
         with open("models/" + height_filename, 'r', encoding='utf-8') as file:
             self.height = int(file.readline().strip())
-
-        #INTEGRATION: call method to load the models here once task is completed
 
     def evaluate_image(self, late=None, dors=None, fron=None, caud=None):
         """
@@ -36,6 +36,92 @@ class EvaluationMethod:
         Returns: Classification of input images and confidence score. 
                 A return of None, -1 indicates an error
         """
+        #define variables outside the if statements so they can be used in other method calls
+        late_confidence_score = 0
+        late_predicted_species = None
+        dors_confidence_score = 0
+        dors_predicted_species = None
+        fron_confidence_score = 0
+        fron_predicted_species = None
+        caud_confidence_score = 0
+        caud_predicted_species = None
+
+        if late:
+            late_image = self.transform_input(late)
+
+            with torch.no_grad():
+                late_output = self.trained_models["late"](late_image)
+
+            # Get the predicted class and confidence score
+            _, predictedIndex = torch.max(late_output, 1)
+            late_confidence_score = torch.nn.functional.softmax(
+                late_output, dim=1)[0][predictedIndex].item()
+            late_predicted_species = predictedIndex.item()
+
+        if dors:
+            #mirrors above usage but for the dors angle
+            dors_image = self.transform_input(dors)
+
+            with torch.no_grad():
+                dors_output = self.trained_models["dors"](dors_image)
+
+            _, predictedIndex = torch.max(dors_output, 1)
+            dors_confidence_score = torch.nn.functional.softmax(
+                dors_output, dim=1)[0][predictedIndex].item()
+            dors_predicted_species = predictedIndex.item()
+
+        if fron:
+            #mirrors above usage but for the fron angle
+            fron_image = self.transform_input(fron)
+
+            with torch.no_grad():
+                fron_output = self.trained_models["fron"](fron_image)
+
+            _, predictedIndex = torch.max(fron_output, 1)
+            fron_confidence_score = torch.nn.functional.softmax(
+                fron_output, dim=1)[0][predictedIndex].item()
+            fron_predicted_species = predictedIndex.item()
+
+        if caud:
+            #mirrors above usage but for the caud angle
+            caud_image = self.transform_input(caud)
+
+            with torch.no_grad():
+                caud_output = self.trained_models["caud"](caud_image)
+
+            _, predictedIndex = torch.max(caud_output, 1)
+            caud_confidence_score = torch.nn.functional.softmax(
+                caud_output, dim=1)[0][predictedIndex].item()
+            caud_predicted_species = predictedIndex.item()
+
+        if self.use_method == 1:
+            use_model = self.heaviest_is_best(fron_confidence_score, dors_confidence_score,
+                                              late_confidence_score, caud_confidence_score)
+
+            #match uses the index returned from the method to decide which prediction to return
+            match use_model:
+                case 0:
+                    return fron_predicted_species, fron_confidence_score
+                case 1:
+                    return dors_predicted_species, dors_confidence_score
+                case 2:
+                    return late_predicted_species, late_confidence_score
+                case 3:
+                    return caud_predicted_species, caud_confidence_score
+                case _:
+                    return None, -1
+
+        elif self.use_method == 2:
+            return self.weighted_eval([fron_confidence_score, dors_confidence_score,
+                                       late_confidence_score, caud_confidence_score],
+                                      [fron_predicted_species, dors_predicted_species,
+                                       late_predicted_species, caud_predicted_species])
+
+        elif self.use_method == 3:
+            return self.stacked_eval()
+
+        else:
+            return None, -1
 
     def heaviest_is_best(self, fron_cert, dors_cert, late_cert, caud_cert):
         """
@@ -102,7 +188,7 @@ class EvaluationMethod:
         Returns: transformed image for classification
         """
         transformation = transforms.Compose([
-            transforms.Resize(self.height), #ResNet expects 224x224 images
+            transforms.Resize((self.height, self.height)), #ResNet expects 224x224 images
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
--- testing/test_evaluation_method.py
+++ testing/test_evaluation_method.py
@@ -2,8 +2,9 @@
 import unittest
 import sys
 import os
-from unittest.mock import patch, mock_open
+from unittest.mock import patch, mock_open, MagicMock
 from PIL import Image
+import torch
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 from evaluation_method import EvaluationMethod
 
@@ -11,11 +12,38 @@ class TestEvaluationMethod(unittest.TestCase):
     """
     Test the evaluation method class methods
     """
+    @patch("builtins.open", new_callable=mock_open, read_data="224")
+    def test_initializer(self, mock_file):
+        """test the initializer for proper setup"""
+        #mock the models
+        mock_models = {
+            "late" : MagicMock(),
+            "fron" : MagicMock(),
+            "dors" : MagicMock(),
+            "caud" : MagicMock()
+        }
+
+        evaluation = EvaluationMethod("height_mock.txt", mock_models)
+
+        mock_file.assert_called_once_with("models/height_mock.txt")
+        self.assertEqual(evaluation.use_method, 1)
+        #Change the weights to match the program's manually
+        self.assertEqual(evaluation.weights, [0.25, 0.25, 0.25, 0.25])
+        self.assertEqual(evaluation.trained_models, mock_models)
+        self.assertEqual(evaluation.height, 224)
+
 
     @patch("builtins.open", new_callable=mock_open, read_data="224")
     def test_heaviest_is_best(self, mock_file):
         """test heaviest is best for proper tracking of highest certainty"""
-        evaluation = EvaluationMethod("height_mock.txt")
+        mock_models = {
+            "late" : MagicMock(),
+            "fron" : MagicMock(),
+            "dors" : MagicMock(),
+            "caud" : MagicMock()
+        }
+
+        evaluation = EvaluationMethod("height_mock.txt", mock_models)
         mock_file.assert_called_once_with("models/height_mock.txt", 'r', encoding='utf-8')
 
         assert evaluation.heaviest_is_best(0.1, 0.3, 0.5, 0.4) == 2
@@ -24,7 +52,14 @@ class TestEvaluationMethod(unittest.TestCase):
     @patch("builtins.open", new_callable=mock_open, read_data="224")
     def test_weighted_eval(self, mock_file):
         """test weighted eval for proper calculation"""
-        evaluation = EvaluationMethod("height_mock.txt")
+        mock_models = {
+            "late" : MagicMock(),
+            "fron" : MagicMock(),
+            "dors" : MagicMock(),
+            "caud" : MagicMock()
+        }
+
+        evaluation = EvaluationMethod("height_mock.txt", mock_models)
         mock_file.assert_called_once_with("models/height_mock.txt", 'r', encoding='utf-8')
         #must be changed if weights are adjusted in code
         given_weights = [0.25, 0.25, 0.25, 0.25]
@@ -38,13 +73,78 @@ class TestEvaluationMethod(unittest.TestCase):
     @patch("builtins.open", new_callable=mock_open, read_data="224")
     def test_transform_input(self, mock_file):
         """test transform input for proper image transformation"""
-        evaluation = EvaluationMethod("height_mock.txt")
+        mock_models = {
+            "late" : MagicMock(),
+            "fron" : MagicMock(),
+            "dors" : MagicMock(),
+            "caud" : MagicMock()
+        }
+
+        evaluation = EvaluationMethod("height_mock.txt", mock_models)
         mock_file.assert_called_once_with("models/height_mock.txt", 'r', encoding='utf-8')
         evaluation.height = 224
         fake_input = Image.new("RGB", (224, 224))
         result = evaluation.transform_input(fake_input)
 
         assert result.shape == (1, 3, 224, 224)
+
+    @patch("evaluation_method.EvaluationMethod.heaviest_is_best", return_value = 0)
+    def test_evaluate_image_uses_heaviest(self, mock_heaviest):
+        """test proper usage when self.use_method = 1"""
+        evaluation = self.create_evaluation_object_for_evaluate_image_tests()
+
+        evaluation.use_method = 1
+        evaluation.evaluate_image(late=torch.rand(3, 224, 224))
+
+        mock_heaviest.assert_called_once()
+
+    @patch("evaluation_method.EvaluationMethod.weighted_eval", return_value = (2, 0.75))
+    def test_evaluate_image_uses_heaviest(self, mock_weighted):
+        """test proper usage when self.use_method = 2"""
+        evaluation = self.create_evaluation_object_for_evaluate_image_tests()
+
+        evaluation.use_method = 2
+        result_species, result_conf = evaluation.evaluate_image(late=torch.rand(3, 224, 224))
+
+        mock_weighted.assert_called_once()
+        self.assertEqual(result_species, 2)
+        self.assertEqual(result_conf, 0.75)
+
+    @patch("evaluation_method.EvaluationMethod.stacked_eval", return_value = (2, 0.75))
+    def test_evaluate_image_uses_heaviest(self, mock_stacked):
+        """test proper usage when self.use_method = 3"""
+        evaluation = self.create_evaluation_object_for_evaluate_image_tests()
+
+        evaluation.use_method = 3
+        result_species, result_conf = evaluation.evaluate_image(late=torch.rand(3, 224, 224))
+
+        mock_stacked.assert_called_once()
+        self.assertEqual(result_species, 2)
+        self.assertEqual(result_conf, 0.75)
+
+    @patch("builtins.open", new_callable=mock_open, read_data="224")
+    def create_evaluation_object_for_evaluate_image_tests(self, mock_file):
+        """
+        Create evaluation object for tests that need redundant
+        versions of this code
+
+        Returns: a set up evaluation object
+        """
+        mock_models = {
+            "late": MagicMock(return_value=torch.tensor([[0.1, 0.3, 0.6]])),
+            "dors": MagicMock(return_value=torch.tensor([[0.2, 0.5, 0.3]])),
+            "fron": MagicMock(return_value=torch.tensor([[0.7, 0.2, 0.1]])),
+            "caud": MagicMock(return_value=torch.tensor([[0.4, 0.4, 0.2]])),
+        }
+
+        evaluation = EvaluationMethod("height_mock.txt", mock_models)
+        mock_file.assert_called_once_with("models/height_mock.txt", 'r', encoding='utf-8')
+
+        #mock transform_input for dummy output
+        mock_transform = MagicMock(return_value = torch.rand(1, 3, 224, 224))
+        evaluation.transform_input = mock_transform
+
+        return evaluation
 
 if __name__ == "__main__":
     unittest.main()
