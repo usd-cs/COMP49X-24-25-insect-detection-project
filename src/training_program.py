aac246f46272507d66305a9c2bdeb8b9e6125ef9
Thomas McKeown <tmckeown@sandiego.edu>
2025-04-08 13:23:52 -0700

    Refined caudal model by adding histogram equalization and increasing the number of epochs

--- src/training_program.py
+++ src/training_program.py
@@ -297,6 +297,7 @@ class TrainingProgram:
         transformation = transforms.Compose([
         transforms.Resize((self.height, self.height)),
         transforms.ToTensor(),
+        HistogramEqualization(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
 
         #save transformation to a file
