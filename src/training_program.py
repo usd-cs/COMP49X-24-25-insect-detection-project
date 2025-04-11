3433d0f37d34cbdef67914ba0f899172dfac7947
Joe Cox <joecox@sandiego.edu>
2025-04-11 10:31:00 -0700

    Frontal model refined - Set Histogram Equalization to its transformation

--- src/training_program.py
+++ src/training_program.py
@@ -77,6 +77,7 @@ class TrainingProgram:
             "fron": transforms.Compose([
         transforms.Resize((self.height, self.height)),
         transforms.ToTensor(),
+        HistogramEqualization(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]),
             "late": transforms.Compose([
         transforms.Resize((self.height, self.height)),
