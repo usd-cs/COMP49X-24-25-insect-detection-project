ba7675487f7f14d324d3da8410eb4b2cb539500c
Joe Cox <joecox@sandiego.edu>
2025-04-30 13:03:10 -0700

    Added class limitig into the database reader

--- src/training_database_reader.py
+++ src/training_database_reader.py
@@ -9,7 +9,7 @@ class DatabaseReader:
     """
     Reads from SQLite database, and creates a pandas dataframe
     """
-    def __init__(self, database, connection=None, table="TrainingData", query=None):
+    def __init__(self, database, connection=None, table="TrainingData", query=None, class_file_path=None):
         """
         Initialize DatabaseReader and loads data into a Pandas DataFrame. 
         
@@ -18,15 +18,41 @@ class DatabaseReader:
             connection (sqlite3.Connection): shared SQLite connection(this is optional).
             table (str): name of the table in the database to query.
             query (str): SQL query to execute(this is optional).
+            class_file_path (str): File path of specified class to be in the models
         """
         self.database = database
         self.connection = connection
         self.table = table
-        # if query is specified, set it to the specified, else set to default query if query=None
-        self.query = query or (
-            f"SELECT Genus, Species, UniqueID, View, SpecimenID, Image FROM {self.table}"
-        )
+
+        if class_file_path:
+            self.allowed_species = self.load_valid_classes(class_file_path)
+            # Use ?'s as placeholders for the species, as they will be fit in later
+            allowed_species = ','.join(['?'] * len(self.allowed_species))
+            self.query = f"""
+                SELECT Genus, Species, UniqueID, View, SpecimenID, Image
+                FROM {self.table}
+                WHERE Species IN ({allowed_species})
+            """
+        else:
+            self.allowed_species = None
+            self.query = query or f"SELECT Genus, Species, UniqueID, View, SpecimenID, Image FROM {self.table}"
+
         self.dataframe = self.load_data()
+
+    def load_valid_classes(self, class_file_path):
+        """
+        Reads in a file containing the valid species entries and initializes the
+        allowed classes set.
+        Returns:
+            Set: the allowed classes to be trained with
+        """
+        with open(class_file_path, 'r', encoding='utf-8') as file:
+            allowed_species = {
+                # Only need the species for class limiting
+                line.strip().split()[-1] for line in file if line.strip()
+            }
+
+        return allowed_species
 
     def load_data(self):
         """
@@ -37,12 +63,13 @@ class DatabaseReader:
             pd.DataFrame: the queried data from the SQLite database as a Pandas DataFrame.
         """
         try:
+            parameters = list(self.allowed_species) if self.allowed_species else []
             if self.connection:
                 # use the provided connection
-                return pd.read_sql_query(self.query, self.connection)
+                return pd.read_sql_query(self.query, self.connection, params=parameters)
             # create a new connection if none is provided
             with sqlite3.connect(self.database) as conn:
-                return pd.read_sql_query(self.query, conn)
+                return pd.read_sql_query(self.query, conn, params=parameters)
         except (sqlite3.DatabaseError, pd.io.sql.DatabaseError) as e:
             # if error is raised, then print error and return empty DataFrame
             print(f"Error reading database: {e}")
