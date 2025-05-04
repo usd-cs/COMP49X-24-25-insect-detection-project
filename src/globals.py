9e84b9356bc02e684c8c0dff77bc553bff5ce2b0
danieldaugbjergusd <ddaugbjerg@sandiego.edu>
2025-05-03 21:24:36 -0700

    added global file to store file names used in simulator

---
+++ src/globals.py
@@ -0,0 +1,21 @@
+""" globals.py """
+
+training_database = "training.db"
+class_list = "src/models/class_list.txt"
+img_height = "height.txt"
+
+# Species Files
+spec_caud_model = "spec_caud.pth"
+spec_dors_model = "spec_dors.pth"
+spec_fron_model = "spec_fron.pth"
+spec_late_model = "spec_late.pth"
+spec_class_dictionary = "spec_dict.json"
+spec_accuracy_list = "spec_accuracies.json"
+
+# Genus Files
+gen_caud_model = "gen_caud.pth"
+gen_dors_model = "gen_dors.pth"
+gen_fron_model = "gen_fron.pth"
+gen_late_model = "gen_late.pth"
+gen_class_dictionary = "gen_dict.json"
+gen_accuracy_list = "gen_accuracies.json"
